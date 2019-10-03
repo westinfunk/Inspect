457170e02ec4d5d514f33ad29dc12a9efb2502ad
westin <westinfunk@gmail.com>
2019-10-02 21:52:37 -0600

    add typescript

---
+++ typescript.js
@@ -0,0 +1,5 @@
+function driver(car) {
+    return "I'm driving a " + car;
+}
+var car = "Honda";
+console.log(driver(car));
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+function driver(car) {
+    return "I'm driving a " + car;
+}
+
+let car = "Honda";
+
+console.log(driver(car));
